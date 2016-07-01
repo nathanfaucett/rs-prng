a1c83c995b0282fdf385fab79f794aff4e530b33
nathanfaucett <nathanfaucett@gmail.com>
2016-07-01 07:51:07 -0500

    api updates, make thread safe

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 #![no_std]
+#![feature(alloc)]
 
 
-pub use core::usize::MAX;
+extern crate alloc;
+
+
+use alloc::arc::Arc;
+use core::cell::RefCell;
+use core::usize::MAX;
 
 
 // https://en.wikipedia.org/wiki/Linear_congruential_generator#Parameters_in_common_use
@@ -17,38 +23,71 @@ static OFFSET: usize = 1442695040888963407;
 static MULTIPLIER: usize = 6364136223846793005;
 
 
-pub struct Random {
+struct RandomData {
     seed: usize,
 }
-
-impl Random {
+impl RandomData {
     #[inline(always)]
-    pub fn new() -> Self {
-        Random {
+    fn new() -> Self {
+        RandomData {
             seed: MAX / 2,
         }
     }
 
+    #[inline(always)]
+    fn seed(&self) -> usize {
+        self.seed
+    }
+    #[inline(always)]
+    fn set_seed(&mut self, s: usize) {
+        self.seed = s;
+    }
+
     // http://indiegamr.com/generate-repeatable-random-numbers-in-js/
     #[inline(always)]
-    pub fn next(&mut self) -> usize {
+    fn next(&mut self) -> usize {
         self.seed = (MULTIPLIER * self.seed + OFFSET) % MAX;
         self.seed
+    }
+}
+
+pub struct Random {
+    data: Arc<RefCell<RandomData>>,
+}
+impl Random {
+    #[inline(always)]
+    pub fn new() -> Self {
+        Random {
+            data: Arc::new(RefCell::new(RandomData::new())),
+        }
     }
 
     #[inline(always)]
     pub fn seed(&self) -> usize {
-        self.seed
+        self.data.borrow().seed()
     }
     #[inline(always)]
-    pub fn set_seed(&mut self, s: usize) {
-        self.seed = s;
+    pub fn set_seed(&self, s: usize) {
+        self.data.borrow_mut().set_seed(s);
+    }
+
+    #[inline(always)]
+    pub fn next(&self) -> usize {
+        self.data.borrow_mut().next()
+    }
+    #[inline(always)]
+    pub fn next_f32(&self) -> f32 {
+        self.next() as f32 / MAX as f32
+    }
+    #[inline(always)]
+    pub fn next_f64(&self) -> f64 {
+        self.next() as f64 / MAX as f64
     }
 }
 
 #[test]
 fn test_random_struct() {
-    let mut random = Random::new();
+    let random = Random::new();
 
     if cfg!(target_pointer_width = "64") {
         assert_eq!(random.next(), 4301930853896946210);
